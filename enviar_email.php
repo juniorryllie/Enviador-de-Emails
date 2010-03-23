a43afcccdea6ee295a1066368ef79e4fb51667e8
Junior Ryllie <junior.ryllie@gmail.com>
2010-03-23 10:26:41 -0300

    Utilizacao de If para condicionar a mensagem e retornar mensagens humanizadas

--- enviar_email.php
+++ enviar_email.php
@@ -1,9 +1,15 @@
 <?php
 
-	$mail = 'junior.ryllie@gmail.com' ;
-	$assunto = $_post['assunto'] ;
-	$corpo = $_post['corpo'] ;
+	$destinatario1 = 'junior.ryllie@gmail.com' ;
+	$assunto1 = $_POST['assunto'] ;
+	$corpo1 = $_POST['corpo'] ;
 	
-	mail($mail, $assunto, $corpo);
+	if (mail($destinatario1, $assunto1, $corpo1)):
+	    
+		print'<p>Mensagem Enviada com Sucesso</p>';
+	else:
+		print'<p>ERRO  Sua Mensagem nao pode ser Enviada </p>';
+	endif;
+	    
 
 ?>
